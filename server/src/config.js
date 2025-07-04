d60a5d4366866e9e525db82f4fd41f70da6d8638
GAM2004 <gustavomoros2004@gmail.com>
2025-07-04 11:04:02 -0400

    Delete server directory

--- server/src/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    port: 3000,
-    db_server: 'LAPTOP\\SQLEXPRESSGAM',
-    db_database: 'Inventario',
-    db_user: 'sa',
-    db_password: 'Admin123!',
-    }
--- server/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import Products from './products';
-import Users from './users';
-import Records from './records'
-const Routes = {
-    Products,
-    Users,
-    Records
-}
-export default Routes;
