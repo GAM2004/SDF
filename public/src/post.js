3d3adb25bd46c780cccfa8acab81d85bc23b083c
GAM2004 <gustavomoros2004@gmail.com>
2025-07-04 11:03:44 -0400

    Delete public directory

--- public/src/post.js
+++
@@ -1,9 +0,0 @@
-export const newProduct = (product, url) => {
-    return fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(product)
-    });
-}
